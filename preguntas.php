411dbd8134014abef93ad44779ffb14dcc8d3f9d
luisccampo <48363026+luisccampo@users.noreply.github.com>
2022-09-04 22:09:40 -0500

    Update preguntas.php

---
+++ preguntas.php
@@ -0,0 +1,15 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Curso de Git y Github desde Cero</h1>
+    <div>
+        <p><b>Revolviendo conflictos en el merge</b></p>
+    </div>
+</body>
+</html>
